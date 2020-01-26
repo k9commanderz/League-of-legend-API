5074d6e3026b2a3f1c63772cb68c7a703c451c4f
abdul mudhir <abdulmudhirss@gmail.com>
2020-01-26 02:16:21 +0000

    added account profile as well as summoners rank

--- RiotAPI.py
+++ RiotAPI.py
@@ -1,41 +1,43 @@
 import requests
+
 """""/lol/champion-mastery/v4/champion-masteries/by-summoner/{encryptedSummonerId}
 summoner/v4/summoners/by-name/k9commanderz  ?api_key=RGAPI-82368e58-5faa-4ba3-b86c-774dea99a8f6"""
 
 services = {"summoner": "/lol/summoner/v4/summoners/by-name/",
             "summoner_mastery": "/lol/champion-mastery/v4/champion-masteries/by-summoner/",
+            "summoner_status": "/lol/league/v4/entries/by-summoner/"
             }
 
-servers = {"BR":"br1.api.riotgames.com",
-           "EUN1": "eun1.api.riotgames.com",
-           "EUW1": "euw1.api.riotgames.com",
-          "JP1": "jp1.api.riotgames.com",
-           ""
-          ,
-"
+servers = {"BR": "br1.api.riotgames.com",
+           "EUN": "eun1.api.riotgames.com",
+           "EUW": "euw1.api.riotgames.com",
+           "JP": "jp1.api.riotgames.com",
+           "KR": "kr.api.riotgames.com",
+           "LA1": "la1.api.riotgames.com",
+           "LA2": "la2.api.riotgames.com",
+           "NA": "na1.api.riotgames.com",
+           "OC": "oc1.api.riotgames.com",
+           "TR": "tr1.api.riotgames.com",
+           "RU": "ru.api.riotgames.com"
            }
 
 
 class Riot_api:
 
-    def __init__(self, service, api_key, user_id, server="euw1"):
-        self.server = server
+    def __init__(self, api_key):
         self.api_key = api_key
-        self.service = service
-        self.user = user_id
-        self.url = "https://{league_server}.api.riotgames.com{api_service}{league_user}/?api_key={Api_key}".format(
+        self.url = None
+
+    def _request(self, service, user_id, server =servers['EUW']):
+        self.server = server
+        self.url = "https://{league_server}{api_service}{league_user}/?api_key={Api_key}".format(
             league_server=self.server,
-            api_service=self.service,
-            league_user=self.user,
+            api_service=service,
+            league_user=user_id,
             Api_key=self.api_key,
 
         )
-
-    def _request(self):
-        return requests.get(self.url).text
+        return requests.get(self.url).json()
 
     def __str__(self):
         return "Makes request based on the difference services provided by riot games"
-
-
-
--- Summoner.py
+++ Summoner.py
@@ -1,18 +1,65 @@
 from RiotAPI import Riot_api as RP
 from RiotAPI import services
+from RiotAPI import servers
 
 
 class Summoner(RP):
+    api_key = "#"
 
-    def __init__(self, user_name, api_key):
-        self.user = user_name
-        self.api = api_key
-        super().__init__(services['summoner'], self.api, self.user)
+    def __init__(self):
+        super().__init__(self.api_key)
+        self.server = None
+        self.summoner_id = None
+        self.user = None
+        self.account_status = None
 
-    def get_summoner(self):
-        pass
+    def get_account_profile(self, username, server=servers['EUW']):
+        """Building up user profile which includes their account profile as well actual game profile"""
+
+        self.user = username.capitalize()
+        self.server = server
+
+        account_data = self._request(services['summoner'], self.user, self.server)
+
+        try:
+            self.account_status = account_data['status']['message']  #receive account status only if an account does not exist
+        except KeyError:
+            self.account_status = "Available"
+
+        if "summoner not found" in self.account_status:
+            return account_data['status']['message']
+        else:
+            account_name = account_data['name']
+            account_id = account_data['accountId']
+            profile_icon = account_data['profileIconId']
+            summoner_level = account_data['summonerLevel']
+            self.summoner_id = account_data['id']
+
+            return account_name, profile_icon, summoner_level, account_id, self.summoner_id
+
+    def get_SR_ranked(self, summoner_id):
+        request_summoner_data = self._request(services["summoner_status"], self.summoner_id, self.server)
+
+        if len(request_summoner_data) == 0:
+            return "Unranked Summoner rift"
+
+        elif "summoner not found" in self.account_status:
+            return "Data not found - summoner not found"
+
+        else:
+            summoner_data = request_summoner_data[0]
+            rank = summoner_data['queueType'], summoner_data['tier'], summoner_data['rank']
+            rank_status = summoner_data['wins'], summoner_data['losses'], summoner_data['hotStreak']
+            rank_promotion = f"Ranked league Point: {summoner_data['leaguePoints']}" if "miniSeries" not in summoner_data else \
+                summoner_data['miniSeries']
+
+            print(rank)
+            print(rank_status)
+            print(rank_promotion)
 
 
-test = Summoner("thechild", "")
-print(test.url)
-print(test.get_json())
+chosen_server = servers['EUW']
+test = Summoner()
+
+print(test.get_account_profile("k9 commanderz", chosen_server))
+print(test.get_SR_ranked(test.summoner_id))
