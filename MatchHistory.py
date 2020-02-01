e904babaf0839b6f3338e2d1b1e8b12da0acdd37
abdul mudhir <abdulmudhirss@gmail.com>
2020-02-01 21:06:43 +0000

    Added Threading to receive total games played for rift and aram

--- MatchHistory.py
+++ MatchHistory.py
@@ -1,5 +1,6 @@
 from RiotAPI import Riot_api as RP
 from RiotAPI import services
+from concurrent.futures import ThreadPoolExecutor
 import json
 
 """
@@ -9,15 +10,14 @@ INPROGRESS BOIS
 """
 
 modes = ["5v5 Draft Pick games",
-"5v5 Ranked Solo games",
-"5v5 Blind Pick games",
-"5v5 Ranked Flex games",
-"Legend of the Poro King games",
-"One For All: Mirror Mode games",
-"5v5 ARAM games",
-"Legend of the Poro King games",
-]
-
+         "5v5 Ranked Solo games",
+         "5v5 Blind Pick games",
+         "5v5 Ranked Flex games",
+         "Legend of the Poro King games",
+         "One For All: Mirror Mode games",
+         "5v5 ARAM games",
+         "Legend of the Poro King games",
+         ]
 
 
 class Match_History:
@@ -27,8 +27,13 @@ class Match_History:
         self.account_id = account_id
         self.server = server
         self.service = service
+        self.aram_games = 0
+        self.rift_games = 0
         self.rift = self.get_queue_id("Summoner's Rift")
         self.aram = self.get_queue_id("Howling Abyss")
+        with ThreadPoolExecutor() as executor:
+            rift_match = executor.map(self.MatchHistoryRift, self.rift)
+            aram_match = executor.map(self.MatchHistoryARAM, self.aram)
 
     def get_map_mode(self, maps_id):
         """
@@ -48,46 +53,39 @@ class Match_History:
             try:
                 if bot:
                     if "Co-op" in map_info['description'] and mode != "Howling Abyss":
-                        map_id.append((map_info['queueId'], map_info['description']))
+                        map_id.append(map_info['queueId'])  # receive Co-op games
                 else:
                     if mode == map_info['map'] and map_info['description'] in modes:
-                        map_id.append((map_info['queueId'], map_info['description']))
+                        map_id.append((map_info['queueId']))  # receive RIFT,ARAM,
             except TypeError:
                 continue
 
         return map_id
 
-    def getMatchHistoryRift(self):
+    def MatchHistoryRift(self, map_id):
         """"
              Get match history including the lanes as well as champions and teams involved 
         """""
-        total_games = 0
-        for map_id, map_name in self.rift:
-            match_list = self.api.o_request(self.service, self.account_id, self.server, f"queue={map_id}",
+        match_list = self.api.o_request(self.service, self.account_id, self.server, f"queue={map_id}",
                                         "beginIndex=100")
-            if 'status' in match_list:  # would mean that no data is found for the map mode
-                continue
-            else:
-                total_games += match_list['totalGames']
-        return total_games
+        if 'status' in match_list:  # would mean that no data is found for the map mode
+            pass
+        else:
+            self.rift_games += match_list['totalGames']
 
-    def getMatchHistoryARAM(self):
+    def MatchHistoryARAM(self, map_id):
         """
 
         Get match history including the lanes as well as champions and teams involved
         :param season: 
         :return: 
         """""
-        total_games = 0
-        for map_id, map_name in self.aram:
-            match_list = self.api.o_request(self.service, self.account_id, self.server, f"queue={map_id}",
+        match_list = self.api.o_request(self.service, self.account_id, self.server, f"queue={map_id}",
                                         "beginIndex=100")
-            if 'status' in match_list:  # would mean that no data is found for the map mode
-                continue
-            else:
-                total_games += match_list['totalGames']
-
-        return total_games
+        if 'status' in match_list:  # would mean that no data is found for the map mode
+            pass
+        else:
+            self.aram_games += match_list['totalGames']
 
     def getLatestMatch(self):
         """
@@ -97,4 +95,3 @@ class Match_History:
         :return: 
         """""
         pass
-
